cb74801db7caf290b18d67dd786df5b9acf614d6
Jan Zich <jzich@emory.edu>
2006-06-20 18:43:19 +0000

    *** empty log message ***
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@153 2440b244-9809-4ec7-bebd-234906d9abf5

--- src/edu/emory/library/tas/web/schema/GroupListBean.java
+++ src/edu/emory/library/tas/web/schema/GroupListBean.java
@@ -1,6 +1,17 @@
 package edu.emory.library.tas.web.schema;
 
+import edu.emory.library.tas.util.query.QueryValue;
+
 public class GroupListBean
 {
+	
+	public Object[] getGroups()
+	{
+		QueryValue query = new QueryValue("Group");
+		Object[] groups = (Object[]) query.executeQuery();
+		if (groups.length == 0) return null;
+		//return Voyage.getGroups();
+		return groups;
+	}
 
 }
