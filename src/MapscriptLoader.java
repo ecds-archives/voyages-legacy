31bedfb9e90264ece05f3421e1fa90f44d379412
Pawel Jurczyk <pjurczy@emory.edu>
2006-07-13 18:36:14 +0000

    *** empty log message ***
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@284 2440b244-9809-4ec7-bebd-234906d9abf5

---
+++ src/MapscriptLoader.java
@@ -0,0 +1,14 @@
+
+public class MapscriptLoader
+{
+    static {
+         try {
+             System.loadLibrary("mapscript"); //load native library
+             System.out.println(" * mapscript native library loaded *");
+         } catch (Exception e) {
+             System.err.println(" * error loading native library *");
+             System.err.println("Error is: " + e);
+             e.printStackTrace();
+         }
+    }
+}
