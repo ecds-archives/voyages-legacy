e6eefde14aac23e8ba0d65be32162f116f354232
Pawel Jurczyk <pjurczy@emory.edu>
2006-07-18 15:21:56 +0000

    *** empty log message ***
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@302 2440b244-9809-4ec7-bebd-234906d9abf5

---
+++ src/edu/emory/library/tas/PortLocation.java
@@ -0,0 +1,38 @@
+package edu.emory.library.tas;
+
+public class PortLocation {
+
+	public String portName;
+	public float x;
+	public float y;
+	
+	public PortLocation() {		
+	}
+
+	public String getPortName() {
+		return portName;
+	}
+
+	public void setPortName(String portName) {
+		this.portName = portName;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+	
+	
+	
+}
