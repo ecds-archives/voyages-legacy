77fca9b740fee3db9d24aaaa55319365bb8140d7
Pawel Jurczyk <pjurczy@emory.edu>
2006-07-18 15:44:50 +0000

    *** empty log message ***
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@304 2440b244-9809-4ec7-bebd-234906d9abf5

--- src/edu/emory/library/tas/GISPortLocation.java
+++ src/edu/emory/library/tas/GISPortLocation.java
@@ -1,5 +1,8 @@
 package edu.emory.library.tas;
 
+import edu.emory.library.tas.util.query.Conditions;
+import edu.emory.library.tas.util.query.QueryValue;
+
 public class GISPortLocation {
 
 	public String portName;
@@ -35,6 +38,18 @@ public class GISPortLocation {
 	
 	public String toString() {
 		return "Port: " + this.getPortName() + " [" + this.x + ", " + this.y + "]";
+	}
+	
+	public static GISPortLocation getGISPortLocation(Dictionary dictionary) {
+		Conditions c = new Conditions();
+		c.addCondition("portName", dictionary.getName(), Conditions.OP_EQUALS);
+		QueryValue qValue = new QueryValue("GISPortLocation", c);
+		Object[] ports = qValue.executeQuery();
+		if (ports.length == 0) {
+			return null;
+		} else {
+			return (GISPortLocation)ports[0];
+		}
 	}
 	
 }
