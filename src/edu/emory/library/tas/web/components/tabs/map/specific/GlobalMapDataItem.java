5a70800920a35d6a162055c2f5fb93cfdd28bc4b
Pawel Jurczyk <pjurczy@emory.edu>
2006-08-04 18:42:58 +0000

    *** empty log message ***
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@390 2440b244-9809-4ec7-bebd-234906d9abf5

--- src/edu/emory/library/tas/web/components/tabs/map/specific/GlobalMapDataItem.java
+++ src/edu/emory/library/tas/web/components/tabs/map/specific/GlobalMapDataItem.java
@@ -4,28 +4,61 @@ import edu.emory.library.tas.web.components.tabs.map.AbstractMapItem;
 import edu.emory.library.tas.web.components.tabs.map.Element;
 import edu.emory.library.tas.web.maps.PointOfInterest;
 
-
+/**
+ * Item that is shown on global map.
+ * This type of map items use circles of different size as symbols on map.
+ * 
+ * @author Pawel Jurczyk
+ *
+ */
 public class GlobalMapDataItem extends AbstractMapItem {
 
+	/**
+	 * Used symbol prefix.
+	 */
 	public static final String SYMBOL_NAME_PREFIX = "circle-";
 	
+	/**
+	 * Name of symbol.
+	 */
 	private String symbolName;
 	
+	/**
+	 * Used color.
+	 */
 	private int color;
 	
-	public GlobalMapDataItem(double x, double y, String mailLabel, int color) {
-		super(x, y, mailLabel);
+	/**
+	 * Data item constructor.
+	 * @param x x coordinate
+	 * @param y y coordinate
+	 * @param mainLabel label 
+	 * @param color color
+	 */
+	public GlobalMapDataItem(double x, double y, String mainLabel, int color) {
+		super(x, y, mainLabel);
 		this.color = color;
 	}
 	
+	/**
+	 * Sets symbol color.
+	 * @param color
+	 */
 	public void setSymbolColor(int color) {
 		this.color = color;
 	}
 
+	/**
+	 * Gets symbol name.
+	 */
 	public String getSymbolName() {
 		return symbolName;
 	}
 	
+	/**
+	 * Sets symbol size. 
+	 * @param size
+	 */
 	public void setSymbolSize(int size) {
 		StringBuffer buffer = new StringBuffer();
 		buffer.append(SYMBOL_NAME_PREFIX).append(this.color);
@@ -33,12 +66,18 @@ public class GlobalMapDataItem extends AbstractMapItem {
 		this.symbolName =  buffer.toString();
 	}
 
+	/**
+	 * Gets tooltip text of map item.
+	 */
 	public PointOfInterest getTooltipText() {
 		PointOfInterest point = new PointOfInterest(this.getProjectedX(), this.getProjectedY());
 		point.setText(this.buildToolTipInfo());
 		return point;
 	}
 	
+	/**
+	 * Builds tooltip location.
+	 */
 	private String buildToolTipInfo() {
 		StringBuffer buffer = new StringBuffer();
 
--- src/edu/emory/library/tas/web/components/tabs/map/specific/GlobalMapDataTransformer.java
+++ src/edu/emory/library/tas/web/components/tabs/map/specific/GlobalMapDataTransformer.java
@@ -16,23 +16,38 @@ import edu.emory.library.tas.web.components.tabs.map.MapItemElement;
 import edu.emory.library.tas.web.components.tabs.map.TransformerResponse;
 
 /**
- * Expects data row: [Dictionary - place] [Number - value] [Integer - color]
+ * Data transformer that transforms DB respnse for global map.
+ * Expects data rows: [Dictionary - place] [Number - value] [Integer - color]
  * 
  * @author Pawe Jurczyk
  * 
  */
 public class GlobalMapDataTransformer extends AbstractDataTransformer {
 
+	/**
+	 * Alowable max circle size.
+	 */
 	private static final int CIRCLE_RANGES = 5;
 
+	/**
+	 * Color of items having more information.
+	 */
 	private static int DOUBLE_COLOR = 5;
 	
+	/**
+	 * Constructs transformer.
+	 * @param map Attributes mapping in data passed to transformData.
+	 */
 	public GlobalMapDataTransformer(AttributesMap map) {
 		super(map);
 	}
 	
+	/**
+	 * Transforms data.
+	 */
 	public TransformerResponse transformData(Object[] data, double min, double max) {
 
+		//Prepare ranges of circles size
 		double[] rangeBoundries = new double[CIRCLE_RANGES + 1];
 		double step = (max - min) / CIRCLE_RANGES;
 		for (int i = 0; i < CIRCLE_RANGES; i++) {
@@ -40,28 +55,45 @@ public class GlobalMapDataTransformer extends AbstractDataTransformer {
 		}
 		rangeBoundries[0] = min;
 
+		//Get items
 		return this.getItems(data, rangeBoundries);
 	}
 
+	/**
+	 * Parses DB result.
+	 * @param data data from DB
+	 * @param ranges ranges of circles size.
+	 * @return
+	 */
 	private TransformerResponse getItems(Object[] data, double[] ranges) {
 
 		List items = new ArrayList();
 
+		//Parse each response row
 		for (int i = 0; i < data.length; i++) {
+			//Get port
 			GISPortLocation gisPort = GISPortLocation.getGISPortLocation((String) ((Object[]) data[i])[0]);
+			
+			//Get color
 			int color = ((Integer) ((Object[]) data[i])[2]).intValue();
+			
+			//Get valaue
 			Number value = (Number) ((Object[]) data[i])[1];
 			if (gisPort != null) {
+				//Create test item
 				GlobalMapDataItem testItem = new GlobalMapDataItem(gisPort.getX(), gisPort.getY(), gisPort
 						.getPortName(), color);
 				int index;
 
+				//Check if test item is among map items that have already been added
 				if ((index = items.indexOf(testItem)) != -1) {
+					//If so - add Element to existing item
 					GlobalMapDataItem item = (GlobalMapDataItem) items.get(index);
 					item.getMapItemElements()[0].addElement(new Element(getAttribute(i, 1), new Double(value
 							.doubleValue())));
 					item.setSymbolColor(DOUBLE_COLOR);
 				} else {
+					//If no - add test item to map items
 					MapItemElement itemElement = new MapItemElement(getAttribute(i, 0));
 					itemElement.addElement(new Element(getAttribute(i, 1), new Double(value.doubleValue())));
 					testItem.addMapItemElement(itemElement);
@@ -73,6 +105,7 @@ public class GlobalMapDataTransformer extends AbstractDataTransformer {
 
 		}
 
+		//Set size of each of items created above
 		for (Iterator iter = items.iterator(); iter.hasNext();) {
 			GlobalMapDataItem dataItem = (GlobalMapDataItem) iter.next();
 
@@ -86,13 +119,18 @@ public class GlobalMapDataTransformer extends AbstractDataTransformer {
 			dataItem.setSymbolSize(index);
 		}
 
+		//Prepare legend
+		//Here legend has 2 groups - sizes and place colors
 		LegendItemsGroup legendSizes = new LegendItemsGroup("Number of slaves");
 		LegendItemsGroup legendColors = new LegendItemsGroup("Places");
+		
+		//Prepare legend about size of dots
 		for (int i = 0; i < CIRCLE_RANGES; i++) {
 			LegendItem item = new LegendItem("symbols/circle-1-" + (i + 1) + ".png", "" + Math.round(ranges[i]) + " - " + Math.round(ranges[i+1]));
 			legendSizes.addItemToGroup(item);
 		}
 		
+		///Prepare legend about colors
 		LegendItem emb = new LegendItem("symbols/circle-" + 1 + "-4.png", "Place of embarkation");
 		LegendItem disemb = new LegendItem("symbols/circle-" + 2 + "-4.png", "Place of disembarkation");
 		LegendItem both = new LegendItem("symbols/circle-" + 5 + "-4.png", "Place of embarkatrion / disembarkation");
@@ -100,6 +138,7 @@ public class GlobalMapDataTransformer extends AbstractDataTransformer {
 		legendColors.addItemToGroup(disemb);
 		legendColors.addItemToGroup(both);
 		
+		//Return result response
 		return new TransformerResponse((AbstractMapItem[]) items.toArray(new AbstractMapItem[] {}), 
 										new LegendItemsGroup[] {legendSizes, legendColors});
 	}
