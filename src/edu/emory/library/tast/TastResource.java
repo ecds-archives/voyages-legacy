adc09f4d348a9097a671540a097e68f6b311ce9f
Pawel Jurczyk <pjurczy@emory.edu>
2007-01-25 13:17:13 +0000

    *** empty log message ***
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@690 2440b244-9809-4ec7-bebd-234906d9abf5

---
+++ src/edu/emory/library/tast/TastResource.java
@@ -0,0 +1,27 @@
+package edu.emory.library.tast;
+
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
+public class TastResource {
+	private static ResourceBundle resource = null;
+	
+	private static synchronized void init() {
+		if (resource == null) {
+			resource = PropertyResourceBundle.getBundle("SlaveTradeResources");
+		}
+	}
+	
+	public static String getText(String text) {
+		if (resource == null) {
+			init();
+		}
+		return resource.getString(text);
+	}
+	
+	public static void main(String[] params) {
+		System.out.println(getText("TEST"));
+		System.out.println(getText("TEST2"));
+	}
+	
+}
