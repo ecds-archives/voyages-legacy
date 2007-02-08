517f68fa72b768d610f323cbf0d0595bf1e3d2fa
Pawel Jurczyk <pjurczy@emory.edu>
2007-02-08 18:19:40 +0000

    *** empty log message ***
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@732 2440b244-9809-4ec7-bebd-234906d9abf5

--- src/edu/emory/library/tast/common/MainMenuBean.java
+++ src/edu/emory/library/tast/common/MainMenuBean.java
@@ -55,7 +55,7 @@ public class MainMenuBean
 										"Images",
 										"resources/images.faces"),
 								new MainMenuBarPageItem(
-										"names",
+										"slaves",
 										"Names Database",
 										"resources/slaves.faces")}),
 						
