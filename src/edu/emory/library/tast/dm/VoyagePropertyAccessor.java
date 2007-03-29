838f6a529e64bbf75b812686155dd6b47b05d654
Jan Zich <jzich@emory.edu>
2007-03-29 14:50:39 +0000

    *** empty log message ***
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@815 2440b244-9809-4ec7-bebd-234906d9abf5

---
+++ src/edu/emory/library/tast/dm/VoyagePropertyAccessor.java
@@ -0,0 +1,21 @@
+package edu.emory.library.tast.dm;
+
+import org.hibernate.PropertyNotFoundException;
+import org.hibernate.property.Getter;
+import org.hibernate.property.PropertyAccessor;
+import org.hibernate.property.Setter;
+
+public class VoyagePropertyAccessor implements PropertyAccessor
+{
+
+	public Getter getGetter(Class theClass, String propertyName) throws PropertyNotFoundException
+	{
+		return new VoyagePropertyGetter(propertyName);
+	}
+
+	public Setter getSetter(Class theClass, String propertyName) throws PropertyNotFoundException
+	{
+		return null;
+	}
+
+}
