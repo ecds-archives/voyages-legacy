5281116664dd350ee4d8b4b2520366c1a92c557e
Pawel Jurczyk <pjurczy@emory.edu>
2007-04-11 18:54:53 +0000

    *** empty log message ***
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@828 2440b244-9809-4ec7-bebd-234906d9abf5

---
+++ src/edu/emory/library/tast/submission/SubmissionAttribute.java
@@ -0,0 +1,50 @@
+package edu.emory.library.tast.submission;
+
+import edu.emory.library.tast.common.grideditor.Value;
+import edu.emory.library.tast.common.grideditor.textbox.TextboxValue;
+import edu.emory.library.tast.dm.attributes.Attribute;
+
+public class SubmissionAttribute {
+	
+	private Attribute attribute[];
+	private String userLabel;
+	private String comment;
+	
+	public SubmissionAttribute(Attribute attr, String userLabel, String comment) {
+		this.attribute = new Attribute[] {attr};
+		this.userLabel = userLabel;
+		this.comment = comment;
+	}
+	
+	public SubmissionAttribute(Attribute[] attrs, String userLabel, String comment) {
+		this.attribute = attrs;
+		this.userLabel = userLabel;
+		this.comment = comment;
+	}
+	
+	public Attribute[] getAttribute() {
+		return attribute;
+	}
+	public void setAttribute(Attribute[] attribute) {
+		this.attribute = attribute;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public String getUserLabel() {
+		return userLabel;
+	}
+	public void setUserLabel(String userLabel) {
+		this.userLabel = userLabel;
+	}
+
+	public Value getValue(Object[] toBeFormatted) {
+		if (toBeFormatted[0] == null) {
+			return new TextboxValue("Empty");
+		}
+		return new TextboxValue(toBeFormatted[0].toString());
+	}
+}
