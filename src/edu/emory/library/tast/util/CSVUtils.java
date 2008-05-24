64464c6f7887ddca04afe6a934937b866ef31cad
Jan Zich <jzich@emory.edu>
2008-05-24 09:56:11 +0000

    Fixing download CSV functionality
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@1399 2440b244-9809-4ec7-bebd-234906d9abf5

--- src/edu/emory/library/tast/util/CSVUtils.java
+++ src/edu/emory/library/tast/util/CSVUtils.java
@@ -1,168 +1,227 @@
 package edu.emory.library.tast.util;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.ScrollableResults;
 import org.hibernate.Session;
 
 import au.com.bytecode.opencsv.CSVWriter;
+import edu.emory.library.tast.AppConfig;
 import edu.emory.library.tast.db.TastDbQuery;
+import edu.emory.library.tast.dm.Dictionary;
+import edu.emory.library.tast.dm.attributes.Attribute;
 
 public class CSVUtils {
 	
-	/*
-	private static class DictionaryInfo {
+	private static class DictionaryInfo
+	{
 		public Class dictionary;
 		public List attributes = new ArrayList();
 	}
-	*/
 
-	/*
-	private static DictionaryInfo[] getAllData(Session sess, QueryValue qValue, ZipOutputStream zipStream, boolean codes , String conditions) throws FileNotFoundException, IOException {
-		
-	    SimpleDateFormat dateFormatter = new SimpleDateFormat(
-	    		AppConfig.getConfiguration().getString(AppConfig.FORMAT_DATE_CVS));
-		
+	private static DictionaryInfo[] getAllData(Session sess, TastDbQuery qValue, ZipOutputStream zipStream, boolean codes, String conditions) throws FileNotFoundException, IOException
+	{
+
+		SimpleDateFormat dateFormatter = new SimpleDateFormat(AppConfig.getConfiguration().getString(AppConfig.FORMAT_DATE_CVS));
+
 		CSVWriter writer = new CSVWriter(new OutputStreamWriter(zipStream), ',');
 		ScrollableResults queryResponse = null;
-		
+
 		Map dictionaries = new HashMap();
-		
-		try {
+
+		try
+		{
 			queryResponse = qValue.executeScrollableQuery(sess);
-			
-			
-			
+
 			Attribute[] populatedAttrs = qValue.getPopulatedAttributes();
-			
-			if (conditions!=""){
-			String[] con= new String[1];			
-			con [0]=conditions;
-			writer.writeNext(con);
+
+			if (conditions != "")
+			{
+				String[] con = new String[1];
+				con[0] = conditions;
+				writer.writeNext(con);
 			}
-			String[] row = new String[populatedAttrs.length-1 ];
-			for (int i = 1; i < populatedAttrs.length; i++) {
-				row[i-1] = populatedAttrs[i].getName();
+			String[] row = new String[populatedAttrs.length - 1];
+			for (int i = 1; i < populatedAttrs.length; i++)
+			{
+				row[i - 1] = populatedAttrs[i].getName();
 			}
-			
+
 			writer.writeNext(row);
 
-			
-			int cnt = 0; // cnt is a counter to indicate the row number of the table
-			
-			while (queryResponse.next()) {
-				
+			int cnt = 0;
+
+			while (queryResponse.next())
+			{
+
 				cnt++;
-				
+
 				Object[] result = queryResponse.get();
-			
+
 				row = new String[populatedAttrs.length - 1];
-				for (int j = 1; j < populatedAttrs.length; j++) {
-					if (result[j] == null) {
+				for (int j = 1; j < populatedAttrs.length; j++)
+				{
+					if (result[j] == null)
+					{
 						row[j - 1] = "";
-					} else {
-						if (!codes) {
+					}
+					else
+					{
+						if (!codes)
+						{
 							if (result[j] instanceof Date)
-								row[j - 1] = dateFormatter.format(result[j]);	
+								row[j - 1] = dateFormatter.format(result[j]);
 							else
-								row[j - 1] = result[j].toString();	
-							if (result[j] instanceof Dictionary) {
-								if (dictionaries.containsKey(populatedAttrs[j].toString())) {
+								row[j - 1] = result[j].toString();
+							if (result[j] instanceof Dictionary)
+							{
+								if (dictionaries.containsKey(populatedAttrs[j].toString()))
+								{
 									DictionaryInfo info = (DictionaryInfo) dictionaries.get(populatedAttrs[j].toString());
-									if (!info.attributes.contains(populatedAttrs[j])) {
+									if (!info.attributes.contains(populatedAttrs[j]))
+									{
 										info.attributes.add(populatedAttrs[j]);
 									}
-								} else {
+								}
+								else
+								{
 									DictionaryInfo info = new DictionaryInfo();
 									info.attributes.add(populatedAttrs[j]);
 									info.dictionary = result[j].getClass();
 									dictionaries.put(populatedAttrs[j].toString(), info);
 								}
 							}
-						} else {
-							if (result[j] instanceof Dictionary) {
-								row[j - 1] = ((Dictionary)result[j]).getId().toString();
-								if (dictionaries.containsKey(populatedAttrs[j].toString())) {
+						}
+						else
+						{
+							if (result[j] instanceof Dictionary)
+							{
+								row[j - 1] = ((Dictionary) result[j]).getId().toString();
+								if (dictionaries.containsKey(populatedAttrs[j].toString()))
+								{
 									DictionaryInfo info = (DictionaryInfo) dictionaries.get(populatedAttrs[j].toString());
-									if (!info.attributes.contains(populatedAttrs[j])) {
+									if (!info.attributes.contains(populatedAttrs[j]))
+									{
 										info.attributes.add(populatedAttrs[j]);
 									}
-								} else {
+								}
+								else
+								{
 									DictionaryInfo info = new DictionaryInfo();
 									info.attributes.add(populatedAttrs[j]);
 									info.dictionary = result[j].getClass();
 									dictionaries.put(populatedAttrs[j].toString(), info);
 								}
-							} else {
+							}
+							else
+							{
 								if (result[j] instanceof Date)
-									row[j - 1] = dateFormatter.format(result[j]);	
+									row[j - 1] = dateFormatter.format(result[j]);
 								else
-									row[j - 1] = result[j].toString();	
+									row[j - 1] = result[j].toString();
 							}
 						}
 					}
 				}
 				writer.writeNext(row);
 			}
-			
-			//this is to show the total number of rows in the downloaded file:
-			writer.writeNext(new String[] {"The number of total records: " + cnt});
+
+			writer.writeNext(new String[] { "The number of total records: " + cnt });
 
 			writer.flush();
 			return (DictionaryInfo[]) dictionaries.values().toArray(new DictionaryInfo[] {});
-			
-		} finally {
-			if (queryResponse != null) {
+
+		}
+		finally
+		{
+			if (queryResponse != null)
+			{
 				queryResponse.close();
 			}
 		}
 	}
-	*/
 
-	public static void writeResponse(Session sess, TastDbQuery qValue, String conditions) {
-		writeResponse(sess, qValue, false,conditions);
+	public static void writeResponse(Session sess, TastDbQuery query, String conditions)
+	{
+		writeResponse(sess, query, false, conditions);
 	}
 	
-	public static void writeResponse(Session sess, TastDbQuery qValue, boolean codes, String conditions) {
+	public static void writeResponse(Session sess, TastDbQuery query)
+	{
+		writeResponse(sess, query, false, "");
+	}
+	
+	public static void writeResponse(Session sess, TastDbQuery query, boolean codes, String conditions)
+	{
 		
 		ZipOutputStream zipOS = null;
 		BufferedReader reader = null;
 		
-		try {
-			FacesContext fc = FacesContext.getCurrentInstance();			
-			HttpServletResponse response = (HttpServletResponse)fc.getExternalContext().getResponse();
+		try
+		{
+			
+			FacesContext fc = FacesContext.getCurrentInstance();
+			HttpServletResponse response = (HttpServletResponse) fc.getExternalContext().getResponse();
+			
 			response.setContentType("application/zip");
 			response.setHeader("content-disposition", "attachment; filename=data.zip");
+			
 			zipOS = new ZipOutputStream(response.getOutputStream());
 			zipOS.putNextEntry(new ZipEntry("data.csv"));
-			//DictionaryInfo[] dicts = getAllData(sess, qValue, zipOS, codes,conditions);
-	//		zipOS.putNextEntry(new ZipEntry("codebook.csv"));
-	//		getDictionaryInfo(zipOS, sess, dicts);
+			
+			getAllData(sess, query, zipOS, codes,conditions);
+			
+			// zipOS.putNextEntry(new ZipEntry("codebook.csv"));
+			// getDictionaryInfo(zipOS, sess, dicts);
+			
 			zipOS.close();
 			fc.responseComplete();
-		} catch (IOException io) {
+
+		}
+		catch (IOException io)
+		{
 			io.printStackTrace();
-		} catch (OutOfMemoryError e) {
+		}
+		catch (OutOfMemoryError e)
+		{
 			e.printStackTrace();
-		} finally {
-			if (reader != null) {
-				try {
+		}
+		finally
+		{
+			if (reader != null)
+			{
+				try
+				{
 					reader.close();
-				} catch (IOException e) {
+				}
+				catch (IOException e)
+				{
 					e.printStackTrace();
 				}
 			}
-			if (zipOS != null) {
-				try {
+			if (zipOS != null)
+			{
+				try
+				{
 					zipOS.flush();
 					zipOS.close();
-				} catch (IOException e) {
+				}
+				catch (IOException e)
+				{
 					e.printStackTrace();
 				}
 			}
@@ -206,51 +265,57 @@ public class CSVUtils {
 	}
 	*/
 
-	public static void writeResponse(Session session, String[][] data) {
+	public static void writeResponse(Session session, String[][] data)
+	{
 		ZipOutputStream zipOS = null;
 		BufferedReader reader = null;
-		
-		try {
-			FacesContext fc = FacesContext.getCurrentInstance();			
-			HttpServletResponse response = (HttpServletResponse)fc.getExternalContext().getResponse();
+
+		try
+		{
+			FacesContext fc = FacesContext.getCurrentInstance();
+			HttpServletResponse response = (HttpServletResponse) fc.getExternalContext().getResponse();
 			response.setContentType("application/zip");
 			response.setHeader("content-disposition", "attachment; filename=data.zip");
 			zipOS = new ZipOutputStream(response.getOutputStream());
 			zipOS.putNextEntry(new ZipEntry("data.csv"));
-			CSVWriter writer = new CSVWriter(new OutputStreamWriter(zipOS), ',');			
-			for (int i = 0; i < data.length; i++) {
-				writer.writeNext(data[i]);			
+			CSVWriter writer = new CSVWriter(new OutputStreamWriter(zipOS), ',');
+			for (int i = 0; i < data.length; i++)
+			{
+				writer.writeNext(data[i]);
 			}
 			writer.close();
 			zipOS.close();
 			fc.responseComplete();
-		} catch (IOException io) {
+		}
+		catch (IOException io)
+		{
 			io.printStackTrace();
-		}finally {
-			if (reader != null) {
-				try {
+		}
+		finally
+		{
+			if (reader != null)
+			{
+				try
+				{
 					reader.close();
-				} catch (IOException e) {
+				}
+				catch (IOException e)
+				{
 					e.printStackTrace();
 				}
 			}
-			if (zipOS != null) {
-				try {
+			if (zipOS != null)
+			{
+				try
+				{
 					zipOS.close();
-				} catch (IOException e) {
+				}
+				catch (IOException e)
+				{
 					e.printStackTrace();
 				}
 			}
 		}
 	}
 
-	public static void writeResponse(Session sess, TastDbQuery q)
-	{
-		writeResponse(sess, q, false,"");
-	}
-	
-	public static void writeResponse(Session sess, TastDbQuery q, boolean codes)
-	{
-		writeResponse(sess, q, codes ,"");
-	}
 }
