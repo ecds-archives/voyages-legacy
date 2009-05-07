4ad25fd09560d899e79c85db3f7e98eb08c1b4a4
Smita Patel <spate62@emory.edu>
2009-05-07 17:40:34 +0000

    initial check-in junit testcase
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@1689 2440b244-9809-4ec7-bebd-234906d9abf5

---
+++ src/TransAtlanticSlaveTrade/src/test/tast/submissions/VoyageCalcTest.java
@@ -0,0 +1,166 @@
+package test.tast.submissions;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.emory.library.tast.db.HibernateConn;
+import edu.emory.library.tast.dm.Fate;
+import edu.emory.library.tast.dm.Port;
+import edu.emory.library.tast.dm.Voyage;
+import edu.emory.library.tast.submission.VoyagesCalculation;
+
+public class VoyageCalcTest extends TestCase {
+	
+	boolean RUN_ALL_MODE = false; 
+	boolean  RUN_SPECIFIC_MODE = true;	
+	Session session = null;
+	Voyage voyage = null;
+	Transaction tran = null;
+
+	@Before
+	public void setUp() throws Exception {	
+		if (session == null) {
+			session = HibernateConn.getSession();
+			tran = session.beginTransaction();
+		}			
+	}
+
+	private void setValuesVoyage(Integer voyageId, String shipName) {
+		voyage = new Voyage();
+		voyage.setVoyageid(new Integer(99900));
+		voyage.setShipname("shipname1");
+		int revision = 1;
+		voyage.setRevision(revision);		
+	}
+	
+	private void deleteVoyage(int voyageId ) {
+		Voyage vNew = Voyage.loadByVoyageId(session, voyageId);	
+		if (vNew != null) {
+			session.delete(vNew);		
+			//tran.commit();
+		}	
+	}
+	
+	private void saveVoyage(Voyage voyage) {
+		session.save(voyage);
+		tran.commit();
+		session.close();		
+	}
+	
+	public VoyageCalcTest(String testName) {
+		super(testName);
+	}	
+	
+	public static TestSuite suite() {
+		return new VoyageCalcTest("testing").invokeTestSuite();
+	}
+	
+	public TestSuite invokeTestSuite() {
+		if (RUN_ALL_MODE) {
+			return runAllTest();
+		}else if (RUN_SPECIFIC_MODE) {
+			return runSpecificTest(); 
+		}
+		else {		
+			return null;
+		}
+	}    
+	
+	public TestSuite runSpecificTest() {
+		//overrides so that test can be executed for localized change
+		System.out.println("Running Specific Tests");
+		TestSuite suite = new TestSuite(this.getClass().getName());
+		//suite.addTest(new VoyageCalcTest("testCalculateImputedValueFate2"));
+		suite.addTest(new VoyageCalcTest("testCalculatePtDepImpByPortdep"));
+		//suite.addTest(new VoyageCalcTest("testCalculatePtDepImpByMajselpt"));
+		
+		return suite;
+	}
+	
+	/*
+	 * For all tests to run successfully, session has to be recreated each time
+	 */
+	public TestSuite runAllTest() {
+		System.out.println("Running All Tests");
+		TestSuite suite = new TestSuite(this.getClass().getName());
+		//add all testcases here
+		suite.addTest(new VoyageCalcTest("testCalculateImputedValueFate2"));
+		suite.addTest(new VoyageCalcTest("testCalculatePtDepImpByPortdep"));
+		suite.addTest(new VoyageCalcTest("testCalculatePtDepImpByMajselpt"));
+		return suite;
+	}
+	
+	@Test
+	public void testCalculatePtDepImpByPortdep(){
+		try {	
+			deleteVoyage(99900);
+			setValuesVoyage(new Integer(99900), "shipName_99900");
+			long portId = 50105;//Rio Amazona
+			Port portdep = Port.loadById(session, portId);			
+			voyage.setPortdep(portdep);
+			VoyagesCalculation voyageCalc = new VoyagesCalculation(voyage, session);			
+			voyageCalc.calculatePtDepImp();
+			saveVoyage(voyage);
+			assertEquals(voyage.getPtdepimp().getId().longValue(), portId);
+			voyage = null;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testCalculatePtDepImpByMajselpt(){
+		try {	
+			deleteVoyage(99901);
+			setValuesVoyage(new Integer(99901), "shipName_99901");
+			long portId = 50420;//Parati
+			Port majselpt = Port.loadById(session, portId);
+			voyage.setMajselpt(majselpt);
+			VoyagesCalculation voyageCalc = new VoyagesCalculation(voyage, session);			
+			voyageCalc.calculatePtDepImp();
+			saveVoyage(voyage);
+			assertEquals(voyage.getPtdepimp().getId().longValue(), portId);
+			voyage = null;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testCalculatePtDepImpByNullValues(){
+		try {	
+			deleteVoyage(99902);
+			setValuesVoyage(new Integer(99902), "shipName_99902");
+			VoyagesCalculation voyageCalc = new VoyagesCalculation(voyage, session);			
+			voyageCalc.calculatePtDepImp();
+			saveVoyage(voyage);
+			voyage = null;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testCalculateImputedValueFate2(){
+		try {	
+			deleteVoyage(99903);
+			setValuesVoyage(new Integer(99903), "shipName_99903");
+			//add test specific variables in voyage object
+			long rigId = 15;
+			Fate fate = Fate.loadById(session, rigId);
+			voyage.setFate(fate);
+			VoyagesCalculation voyageCalc = new VoyagesCalculation(voyage, session);			
+			voyageCalc.calculateImputedValueFate2();
+			saveVoyage(voyage);
+			voyage = null;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
