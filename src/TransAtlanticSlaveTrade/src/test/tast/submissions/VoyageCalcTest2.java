6d273fa496b799a42384b446af65af3142bda775
Alex Thomas <athom09@emory.edu>
2009-05-11 14:18:16 +0000

    Add junit tests for helper methods
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@1698 2440b244-9809-4ec7-bebd-234906d9abf5

---
+++ src/TransAtlanticSlaveTrade/src/test/tast/submissions/VoyageCalcTest2.java
@@ -0,0 +1,253 @@
+package test.tast.submissions;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.emory.library.tast.db.HibernateConn;
+import edu.emory.library.tast.dm.Fate;
+import edu.emory.library.tast.dm.Port;
+import edu.emory.library.tast.dm.Voyage;
+import edu.emory.library.tast.submission.VoyagesCalculation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+public class VoyageCalcTest2 extends TestCase {
+	
+	//boolean RUN_ALL_MODE = false; 
+	//boolean  RUN_SPECIFIC_MODE = true;	
+	
+	Session session = null;
+	Voyage voyage = null;
+	Transaction tran = null;
+	
+	@Before
+	public void setUp() throws Exception {	
+		if (session == null) {
+			session = HibernateConn.getSession();
+			tran = session.beginTransaction();
+		}			
+	}
+	
+
+	private void setValuesVoyage(Integer voyageId, String shipName) {
+		voyage = new Voyage();
+		voyage.setVoyageid(voyageId);
+		voyage.setShipname(shipName);
+		int revision = 1;
+		voyage.setRevision(revision);		
+	}
+	
+	private void deleteVoyage(int voyageId ) {
+		Voyage vNew = Voyage.loadByVoyageId(session, voyageId);	
+		if (vNew != null) {
+			session.delete(vNew);		
+			//tran.commit();
+		}	
+	}
+	
+	private void saveVoyage(Voyage voyage) {
+		session.save(voyage);
+		tran.commit();
+		session.close();		
+	}
+	
+	public VoyageCalcTest2(String testName) {
+		super(testName);
+	}	
+	/*
+	public static TestSuite suite() {
+		return new VoyageCalcTest("testing").invokeTestSuite();
+	}
+	
+	public TestSuite invokeTestSuite() {
+		if (RUN_ALL_MODE) {
+			return runAllTest();
+		}else if (RUN_SPECIFIC_MODE) {
+			return runSpecificTest(); 
+		}
+		else {		
+			return null;
+		}
+	}    
+	
+	public TestSuite runSpecificTest() {
+		//overrides so that test can be executed for localized change
+		System.out.println("Running Specific Tests");
+		TestSuite suite = new TestSuite(this.getClass().getName());
+		//suite.addTest(new VoyageCalcTest("testCalculateImputedValueFate2"));
+		//suite.addTest(new VoyageCalcTest("testCalculatePtDepImpByPortdep"));
+		//suite.addTest(new VoyageCalcTest("testCalculatePtDepImpByMajselpt"));
+		//suite.addTest(new VoyageCalcTest("testCalculatePtDepImpByNullValues"));	
+		//suite.addTest(new VoyageCalcTest("testCalculateTslmtimp"));
+		//suite.addTest(new VoyageCalcTest("testCalculateTslmtimpWithAllVars"));
+		suite.addTest(new VoyageCalcTest("testTest"));
+		
+			
+		return suite;
+	}
+	
+	/*
+	 * For all tests to run successfully, session has to be recreated each time
+	 * /
+	public TestSuite runAllTest() {
+		System.out.println("Running All Tests");
+		TestSuite suite = new TestSuite(this.getClass().getName());
+		//add all testcases here
+		suite.addTest(new VoyageCalcTest("testCalculateImputedValueFate2"));
+		suite.addTest(new VoyageCalcTest("testCalculatePtDepImpByPortdep"));
+		suite.addTest(new VoyageCalcTest("testCalculatePtDepImpByMajselpt"));
+		return suite;
+	}
+	*/
+	
+	
+	
+	//Helper Functions
+	//recode
+	public void testDefValInt()
+	{
+	  Integer I1 =null;
+	  Integer I2=10;
+	  Integer result=null;
+	  VoyagesCalculation voyageCalc = new VoyagesCalculation(voyage, session);
+	  
+	   result= voyageCalc.defVal(I1, 5);
+	   assertEquals((Integer)result, (Integer)5);
+	   
+	   result= voyageCalc.defVal(I2, 1);
+	   assertEquals(result, (Integer)10);
+	}
+	
+	public void testDefValDouble()
+	{
+	  Double D1 =null;
+	  Double D2=10.5;
+	  Double result=null;
+	  VoyagesCalculation voyageCalc = new VoyagesCalculation(voyage, session);
+	  
+	   result= voyageCalc.defVal(D1, 5.5);
+	   assertEquals(result, 5.5);
+	   
+	   result= voyageCalc.defVal(D2, 1d);
+	   assertEquals(result, 10.5);
+	}
+	
+	public void testRoundLong()
+	{
+		Long L1=5l;
+		Long L2=null;
+		Long result=null;
+		
+		 VoyagesCalculation voyageCalc = new VoyagesCalculation(voyage, session);
+		 
+		 result=voyageCalc.round(L1);
+		 assertEquals(result, (Long)5l);
+		 
+		 result=voyageCalc.round(L2);
+		 assertNull(result);
+	}
+	
+	public void testRoundDouble()
+	{
+		Double D1=5d;
+		Double D2=10.5;
+		Double D3=20.7;
+		Double D4=20.3;
+		Double D5=null;
+		Double result=null;
+		
+		 VoyagesCalculation voyageCalc = new VoyagesCalculation(voyage, session);
+		 
+		 result=voyageCalc.round(D1);
+		 assertEquals(result, (Double)5d);
+		 
+		 result=voyageCalc.round(D2);
+		 assertEquals(result, (Double)11d);
+		 
+		 result=voyageCalc.round(D3);
+		 assertEquals(result, (Double)21d);
+		 
+		 result=voyageCalc.round(D4);
+		 assertEquals(result, (Double)20d);
+		 
+		 result=voyageCalc.round(D5);
+		 assertNull(result);
+	}
+	
+	public void testDateDiff()
+	{
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		
+		
+		try {
+			Date D1 = sdf.parse("2009-05-11 10:00");
+			Date D2 = sdf.parse("2009-05-16 20:00");
+			
+			VoyagesCalculation voyageCalc = new VoyagesCalculation(voyage, session);
+			
+			Integer result = voyageCalc.dateDiff(D1, D2);
+			assertEquals(result, (Integer)5);
+			
+			result = voyageCalc.dateDiff(D2, D1);
+			assertEquals(result, new Integer("5"));
+			
+			result = voyageCalc.dateDiff(D1, null);
+			assertNull(result);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void testRecode()
+	{
+		VoyagesCalculation voyageCalc = new VoyagesCalculation(voyage, session);
+		
+		//Include Endpoints
+		ArrayList inputs = new ArrayList();
+		inputs.add(new Integer ("10"));
+		inputs.add(new Integer ("15"));
+		inputs.add(new Integer ("35"));
+		inputs.add(new Integer ("0"));
+		
+		ArrayList ranges = new ArrayList();
+		ranges.add(new Integer[]{new Integer("1"), new Integer("10"), new Integer("100")});
+		ranges.add(new Integer[]{new Integer("11"), new Integer("20"), new Integer("200")});
+		ranges.add(new Integer[]{new Integer("21"), new Integer("35"), new Integer("300")});
+		
+		ArrayList results = voyageCalc.recode(inputs, ranges, true);
+		
+		assertEquals((Integer)results.get(0), new Integer("100"));
+		assertEquals((Integer)results.get(1), new Integer("200"));
+		assertEquals((Integer)results.get(2), new Integer("300"));
+		assertEquals((Integer)results.get(3), new Integer("-1"));
+		
+		//Do Not Include Endpoints
+		inputs = new ArrayList();
+		inputs.add(new Integer ("10"));
+		inputs.add(new Integer ("15"));
+		inputs.add(new Integer ("35"));
+		inputs.add(new Integer ("0"));
+		
+		ranges = new ArrayList();
+		ranges.add(new Integer[]{new Integer("1"), new Integer("10"), new Integer("100")});
+		ranges.add(new Integer[]{new Integer("11"), new Integer("20"), new Integer("200")});
+		ranges.add(new Integer[]{new Integer("21"), new Integer("36"), new Integer("300")});
+		
+		results = voyageCalc.recode(inputs, ranges, false);
+		
+		assertEquals((Integer)results.get(0), new Integer("-1"));
+		assertEquals((Integer)results.get(1), new Integer("200"));
+		assertEquals((Integer)results.get(2), new Integer("300"));
+		assertEquals((Integer)results.get(3), new Integer("-1"));
+		
+	}
+	
+}
