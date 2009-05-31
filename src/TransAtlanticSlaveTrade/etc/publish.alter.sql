ca4be95fd61fc53f9433523b0c42faa6d28db0e3
Alex Thomas <athom09@emory.edu>
2009-05-31 07:59:49 +0000

    Changes for publish process
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@1746 2440b244-9809-4ec7-bebd-234906d9abf5

---
+++ src/TransAtlanticSlaveTrade/etc/publish.alter.sql
@@ -0,0 +1,21 @@
+ALTER TABLE submissions_edited_voyages DROP CONSTRAINT fk_sev_to_voyages;
+ALTER TABLE submissions_edited_voyages   ADD CONSTRAINT fk_sev_to_voyages FOREIGN KEY (voyage_iid) REFERENCES voyages (iid) ON DELETE CASCADE;      
+
+ALTER TABLE submissions_edit DROP CONSTRAINT fk_editor_old_voyage_submissions_edit_to_submissions_edited_voy;
+ALTER TABLE submissions_edit ADD CONSTRAINT fk_editor_old_voyage_submissions_edit_to_submissions_edited_voy FOREIGN KEY (old_edited_voyage_id) REFERENCES submissions_edited_voyages (id)  ON DELETE CASCADE;
+
+ALTER TABLE submission_editors DROP CONSTRAINT fk_submission_users_to_edited_voyages;
+ALTER TABLE submission_editors ADD CONSTRAINT fk_submission_users_to_edited_voyages FOREIGN KEY (edited_voyage_id) REFERENCES submissions_edited_voyages (id)  ON DELETE CASCADE;
+
+ALTER TABLE submissions_edit DROP CONSTRAINT fk_editor_edited_voyage_submissions_edit_to_submissions_edited_;
+ALTER TABLE submissions_edit ADD CONSTRAINT fk_editor_edited_voyage_submissions_edit_to_submissions_edited_ FOREIGN KEY (editor_edited_voyage_id) REFERENCES submissions_edited_voyages (id)  ON DELETE CASCADE;
+
+ALTER TABLE submissions_edit DROP CONSTRAINT fk_new_edited_voyage_submissions_edit_to_submissions_edited_voy;
+ALTER TABLE submissions_edit ADD CONSTRAINT fk_new_edited_voyage_submissions_edit_to_submissions_edited_voy FOREIGN KEY (new_edited_voyage_id) REFERENCES submissions_edited_voyages (id)  ON DELETE CASCADE;
+
+ALTER TABLE submissions_new DROP CONSTRAINT fk_editor_edited_voyage_submissions_new_to_submissions_edited_v;
+ALTER TABLE submissions_new ADD CONSTRAINT fk_editor_edited_voyage_submissions_new_to_submissions_edited_v FOREIGN KEY (editor_edited_voyage_id) REFERENCES submissions_edited_voyages (id)  ON DELETE CASCADE;
+
+ALTER TABLE submissions_new DROP CONSTRAINT fk_new_edited_voyage_submissions_new_to_submissions_edited_voya;
+ALTER TABLE submissions_new ADD CONSTRAINT fk_new_edited_voyage_submissions_new_to_submissions_edited_voya FOREIGN KEY (new_edited_voyage_id) REFERENCES submissions_edited_voyages (id)  ON DELETE CASCADE;
+
--- src/TransAtlanticSlaveTrade/etc/publish.sql
+++ src/TransAtlanticSlaveTrade/etc/publish.sql
@@ -6,63 +6,22 @@ CREATE OR REPLACE FUNCTION publish(revisionname character varying)
   RETURNS boolean AS
 $BODY$
 DECLARE
-	revID integer; --Next Revision
-	curRevID integer; --Current Revision
         voyID integer; --VoyageId that is currently being processed  
-        
         voyRec voyages%rowtype;  -- Record for FOR LOOP 1
-        voyRec2 voyages%rowtype;  -- Record for FOR LOOP 2
-        voyIID integer; --VoyageIId for LOOP 1
-        eID integer; --Edit ID for LOOP 1
-        
 BEGIN
         RAISE NOTICE '----------START----------';
-
-        --Get Next Revision
-	SELECT nextval('revisions_sequence') INTO revID;
-	RAISE NOTICE 'Next Revision: %', revID;
-
-	--Get Current Revision
-	SELECT max(revision) INTO curRevID FROM voyages;
-	RAISE NOTICE 'Current Revision: %', curRevID;
-
-        --Create new revision record in revisions table
-	INSERT INTO revisions (id, name) values (revID, revisionName);
-	RAISE NOTICE 'Created New Revision Record: % %', revID, revisionName;
-
-      --Remove constraints and indexes 
-      select publishpreupdate();
-
-	--Update revision to next number
-        RAISE NOTICE 'Start Update';
-        UPDATE voyages SET revision=revID WHERE revision > 0 AND suggestion='f'; 
-        RAISE NOTICE 'End Update';
-
-        --Restore constraints and indexes
-        select publishpostupdate();
-
 	
-        --Update and delete records for new revision
-        FOR voyRec IN SELECT voyageid FROM voyages v WHERE  revision=-1 AND suggestion='f' 
+        FOR voyRec IN SELECT * FROM voyages v WHERE  revision=-1 AND suggestion='f' 
         LOOP
              voyID:=voyRec.voyageid;
              RAISE NOTICE 'Processing voyageId: %', voyID;
              
-             UPDATE voyages SET revision=revID WHERE revision=-1 and suggestion='f' AND voyageid=voyID;
-             RAISE NOTICE 'After Update';
-             
-             
              --Delete voyage record an FKs
-	     FOR voyRec2 IN SELECT iid FROM voyages  WHERE voyageid=voyID AND ( (revision=-1 AND suggestion='f') OR (revision=0) ) 	     LOOP
-	     voyIID:=voyRec2.iid;
-	     SELECT id INTO eID FROM submissions_edited_voyages where voyage_iid= voyIID;
+	     DELETE FROM voyages WHERE voyageid=voyID AND ( (revision=0) OR (revision=1 AND suggestion='f'));
 
-             DELETE FROM submissions_edit WHERE old_edited_voyage_id=eID;
-	     DELETE FROM submissions_edited_voyages where voyage_iid= voyIID;
-	     DELETE FROM voyages  WHERE voyageid=voyID AND ( (revision=curRevID  AND  suggestion='f') OR (revision=0) );
-	     END LOOP;
-
-	     RAISE NOTICE 'After Delete';
+             --Update to active revision  
+	     UPDATE voyages SET revision=1 WHERE voyageid=voyID AND revision=-1 and suggestion='f';
+             RAISE NOTICE 'After Update';
         END LOOP;
 
 	 
@@ -70,9 +29,9 @@ BEGIN
         
 	RETURN true;
 	
-	EXCEPTION
-	WHEN OTHERS THEN
-	    ROLLBACK;
+	--EXCEPTION
+	--WHEN OTHERS THEN
+	    --ROLLBACK;
 END; 
 $BODY$
   LANGUAGE 'plpgsql' VOLATILE
