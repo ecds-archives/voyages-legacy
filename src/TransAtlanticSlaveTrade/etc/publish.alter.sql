cbd71814ed2dabf4e30c080201bf70e875487a03
Alex Thomas <athom09@emory.edu>
2009-06-01 21:08:17 +0000

    Modified constraint for merging
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@1749 2440b244-9809-4ec7-bebd-234906d9abf5

--- src/TransAtlanticSlaveTrade/etc/publish.alter.sql
+++ src/TransAtlanticSlaveTrade/etc/publish.alter.sql
@@ -19,3 +19,11 @@ ALTER TABLE submissions_new ADD CONSTRAINT fk_editor_edited_voyage_submissions_n
 ALTER TABLE submissions_new DROP CONSTRAINT fk_new_edited_voyage_submissions_new_to_submissions_edited_voya;
 ALTER TABLE submissions_new ADD CONSTRAINT fk_new_edited_voyage_submissions_new_to_submissions_edited_voya FOREIGN KEY (new_edited_voyage_id) REFERENCES submissions_edited_voyages (id)  ON DELETE CASCADE;
 
+ALTER TABLE submissions_merge DROP CONSTRAINT fk_editor_edited_voyage_submissions_merge_to_submissions_edited;
+ALTER TABLE submissions_merge ADD CONSTRAINT fk_editor_edited_voyage_submissions_merge_to_submissions_edited FOREIGN KEY (editor_edited_voyage_id) REFERENCES submissions_edited_voyages (id) ON DELETE CASCADE;
+
+ALTER TABLE submissions_merge DROP CONSTRAINT fk_proposed_edited_voyage_submissions_merge_to_submissions_edit;
+ALTER TABLE submissions_merge ADD CONSTRAINT fk_proposed_edited_voyage_submissions_merge_to_submissions_edit FOREIGN KEY (proposed_edited_voyage_id) REFERENCES submissions_edited_voyages (id) ON DELETE CASCADE;
+
+ALTER TABLE submissions_merge_voyages DROP CONSTRAINT fk_edited_voyage_submissions_merge_voyages_to_submissions_edite;
+ALTER TABLE submissions_merge_voyages ADD CONSTRAINT fk_edited_voyage_submissions_merge_voyages_to_submissions_edite FOREIGN KEY (edited_voyage_id) REFERENCES submissions_edited_voyages (id) ON DELETE CASCADE;
