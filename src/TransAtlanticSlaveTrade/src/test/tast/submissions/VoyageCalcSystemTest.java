2b5b3e201f65d414024c8315c6774c696a025a9d
Smita Patel <spate62@emory.edu>
2009-06-30 14:26:07 +0000

    Added for batch testing
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@1812 2440b244-9809-4ec7-bebd-234906d9abf5

---
+++ src/TransAtlanticSlaveTrade/src/test/tast/submissions/VoyageCalcSystemTest.java
@@ -0,0 +1,211 @@
+package test.tast.submissions;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import org.junit.Test;
+
+import edu.emory.library.tast.db.HibernateConn;
+import edu.emory.library.tast.dm.Fate;
+import edu.emory.library.tast.dm.Nation;
+import edu.emory.library.tast.dm.Port;
+import edu.emory.library.tast.dm.VesselRig;
+import edu.emory.library.tast.dm.Voyage;
+import edu.emory.library.tast.submission.VoyagesCalculation;
+
+public class VoyageCalcSystemTest extends TestCase {
+	
+	boolean RUN_ALL_MODE = false; 
+	boolean  RUN_SPECIFIC_MODE = true;	
+	Session session = null;
+	Transaction tran = null;
+
+	public void setUpSession() throws Exception {
+			session = HibernateConn.getSession();
+			tran = session.beginTransaction();
+	}
+	
+	private void deleteVoyage(int voyageId ) {
+		Voyage vNew = Voyage.loadByVoyageId(session, voyageId);	
+		if (vNew != null) {
+			session.delete(vNew);		
+			//tran.commit();
+		}	
+	}
+	
+	private void saveVoyage(Voyage voyage) {
+		session.save(voyage);
+		tran.commit();
+		session.close();		
+	}
+	
+	private void initAllValuesVoyage(Voyage voy) {
+		voy.setFate2(null);
+		voy.setFate3(null);		 
+		voy.setFate4(null);
+		voy.setTonmod(null);
+		voy.setMajbuypt(null);			
+		voy.setMajselpt(null);
+		voy.setYear5(null);
+		voy.setYear10(null);
+		voy.setYear25(null);
+		voy.setYear100(null);
+		voy.setNatinimp(null);
+		voy.setVoy1imp(null);
+		voy.setVoy2imp(null);
+		voy.setPtdepimp(null);
+		voy.setTslmtimp(null);
+		voy.setVymrtrat(null);
+		voy.setVymrtimp(null);
+		voy.setConstreg(null);	    
+		voy.setRegisreg(null);	    
+		voy.setDeptreg(null);	    
+		voy.setDeptregimp(null);	    
+		voy.setEmbreg(null);	    
+		voy.setEmbreg2(null);	    
+		voy.setRegem1(null);	    
+		voy.setRegem2(null);	    
+		voy.setRegem3(null);	    
+		voy.setMajbuyreg(null);	    
+		voy.setMajbyimp(null);	    
+		voy.setRegarr(null);	    
+		voy.setRegarr2(null);	    
+		voy.setRegdis1(null);	    
+		voy.setRegdis2(null);	    
+		voy.setRegdis3(null);	    
+		voy.setMajselrg(null);	    
+		voy.setMjselimp(null);	    
+		voy.setRetrnreg(null);	    
+		voy.setDeptreg1(null);	    
+		voy.setDeptregimp1(null);	    
+		voy.setMajbyimp1(null);	    
+		voy.setMjselimp1(null);	    
+		voy.setRetrnreg1(null);	    
+		voy.setSlaximp(null);
+		voy.setSlamimp(null);
+		voy.setAdlt1imp(null); 
+		voy.setChil1imp(null);
+		voy.setMale1imp(null);
+		voy.setFeml1imp(null);
+		voy.setAdlt2imp(null);
+		voy.setChil2imp(null);
+		voy.setMale2imp(null);
+		voy.setFeml2imp(null);
+		voy.setSlavema1(null);
+		voy.setSlavemx1(null);
+		voy.setSlavmax1(null);
+		voy.setChilrat1(null);
+		voy.setMalrat1(null);  
+		voy.setMenrat1(null);
+		voy.setWomrat1(null);
+		voy.setBoyrat1(null);
+		voy.setGirlrat1(null);
+		voy.setAdlt3imp(null);
+		voy.setChil3imp(null);
+		voy.setMale3imp(null);
+		voy.setFeml3imp(null);
+		voy.setSlavema3(null);
+		voy.setSlavemx3(null);
+		voy.setSlavmax3(null);
+		voy.setChilrat3(null);
+		voy.setMalrat3(null);
+		voy.setMenrat3(null);
+		voy.setWomrat3(null);
+		voy.setBoyrat3(null);
+		voy.setGirlrat3(null);
+		voy.setSlavema7(null);
+		voy.setSlavemx7(null);
+		voy.setSlavmax7(null);
+		voy.setMen7(null);
+		voy.setWomen7(null);
+		voy.setBoy7(null);
+		voy.setGirl7(null);
+		voy.setAdult7(null);
+		voy.setChild7(null);
+		voy.setMale7(null);
+		voy.setFemale7(null);
+		voy.setMenrat7(null);
+		voy.setWomrat7(null);
+		voy.setBoyrat7(null);
+		voy.setGirlrat7(null);
+		voy.setMalrat7(null);
+		voy.setChilrat7(null);
+		voy.setXmimpflag(null);
+		voy.setMjbyptimp(null);
+		voy.setMjslptimp(null);
+		voy.setYearam(null);
+		voy.setYearaf(null);
+		voy.setYeardep(null);				
+		voy.setDatedep(null);
+		voy.setDatebuy(null);
+		voy.setDateleftafr(null);
+		voy.setDateland1(null);
+		voy.setDateland2(null);
+		voy.setDateland3(null);
+		voy.setDateend(null);
+		voy.setDatedepam(null);
+	}
+	public VoyageCalcSystemTest(String testName) {
+		super(testName);
+	}	
+	
+	public static TestSuite suite() {
+		return new VoyageCalcSystemTest("testing").invokeTestSuite();
+	}
+	
+	public TestSuite invokeTestSuite() {
+		if (RUN_ALL_MODE) {
+			return runAllTest();
+		}else if (RUN_SPECIFIC_MODE) {
+			return runSpecificTest(); 
+		}
+		else {		
+			return null;
+		}
+	}    
+	
+	public TestSuite runSpecificTest() {
+		//overrides so that test can be executed for localized change
+		System.out.println("Running Specific Tests");
+		TestSuite suite = new TestSuite(this.getClass().getName());
+		suite.addTest(new VoyageCalcSystemTest("testImputedVars"));
+		
+		return suite;
+	}
+	
+	/*
+	 * For all tests to run successfully, session has to be recreated each time
+	 */
+	public TestSuite runAllTest() {
+		System.out.println("Running All Tests");
+		TestSuite suite = new TestSuite(this.getClass().getName());
+		//add all testcases here
+		return suite;
+	}	
+	
+	@Test
+	public void testImputedVars(){
+		try{
+			setUpSession();		
+			int voyageId = 28;			
+			int revision = 1;						
+			Voyage voy = Voyage.loadByVoyageId(session, voyageId, revision);
+			initAllValuesVoyage(voy);
+			System.out.println("voyage before: " + voy.toString());
+			VoyagesCalculation voyageCalc = new VoyagesCalculation(session, voy);	
+			Voyage voy1 = voyageCalc.calculateImputedVariables();
+			tran.commit();
+			session.close();
+			System.out.println("voyage after: " + voy1.toString());
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
