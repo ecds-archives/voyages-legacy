01d6fc4079b7f57a9676948c0158da3adb2db772
Smita Patel <spate62@emory.edu>
2009-09-01 17:53:45 +0000

    added test case
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@1936 2440b244-9809-4ec7-bebd-234906d9abf5

--- src/TransAtlanticSlaveTrade/src/test/tast/submissions/DbExportTest.java
+++ src/TransAtlanticSlaveTrade/src/test/tast/submissions/DbExportTest.java
@@ -9,10 +9,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import org.junit.Test;
 
 import au.com.bytecode.opencsv.CSVWriter;
@@ -24,8 +29,10 @@ import edu.emory.library.tast.db.TastDbConditions;
 import edu.emory.library.tast.db.TastDbQuery;
 import edu.emory.library.tast.dm.EditedVoyage;
 import edu.emory.library.tast.dm.Submission;
+import edu.emory.library.tast.dm.SubmissionEdit;
 import edu.emory.library.tast.dm.SubmissionNew;
 import edu.emory.library.tast.dm.Voyage;
+import edu.emory.library.tast.dm.attributes.Attribute;
 import edu.emory.library.tast.submission.AdminSubmissionBean;
 import edu.emory.library.tast.util.CSVUtils;
 
@@ -61,9 +68,11 @@ public class DbExportTest extends TestCase{
 		System.out.println("Running Specific Tests");
 		TestSuite suite = new TestSuite(this.getClass().getName());
 
-		//suite.addTest(new DbExportTest("testBuildSubmissionQuery"));
-	//	suite.addTest(new DbExportTest("testSaveFile"));
-		suite.addTest(new DbExportTest("testAdminBean"));
+		suite.addTest(new DbExportTest("testBuildSubmissionQuery"));
+	    //suite.addTest(new DbExportTest("testSaveFile"));
+		//suite.addTest(new DbExportTest("testAdminBean"));
+		//suite.addTest(new DbExportTest("testDbExport"));
+		
 			
 		//suite.addTest(new DbExportTest("testAccentCharacters"));
 				
@@ -97,7 +106,7 @@ public class DbExportTest extends TestCase{
 			long startTime = System.currentTimeMillis();
 			
 			TastDbQuery query = getQuery();
-			CSVUtils.writeResponse(session, query, false, true);
+			//CSVUtils.writeResponse(session, query, false, true);
 			tran.commit();
 			session.close();
 			long endTime = System.currentTimeMillis();
@@ -110,7 +119,7 @@ public class DbExportTest extends TestCase{
 	
 	private TastDbQuery getQuery() {
 		long startTime = System.currentTimeMillis();
-		TastDbConditions cond = new TastDbConditions();
+		/*TastDbConditions cond = new TastDbConditions();
 		int revision = -1;
 		cond.addCondition(Voyage.getAttribute("revision"), new Integer(revision), TastDbConditions.OP_EQUALS);
 		TastDbQuery q = new TastDbQuery(new String[] {"Voyage"}, new String[] {}, cond, 1000);
@@ -119,7 +128,24 @@ public class DbExportTest extends TestCase{
 		for (int i = 0; i < attrs.length; i++) {
 			q.addPopulatedAttribute(Voyage.getAttribute(attrs[i]));
 		}
-		return q;
+		return q;*/
+		Integer[] revArr = {1, -1};
+		TastDbConditions cond = new TastDbConditions(TastDbConditions.AND);
+		cond.addCondition(Voyage.getAttribute("revision"), revArr, TastDbConditions.OP_IN);
+		cond.addCondition(Voyage.getAttribute("suggestion"), new Boolean(false), TastDbConditions.OP_EQUALS);
+		TastDbQuery query = new TastDbQuery("Voyage", cond);
+		query.setOrderBy(new Attribute[] {Voyage.getAttribute("voyageid")});
+		query.setOrder(TastDbQuery.ORDER_ASC);
+		String[] str = 
+			{"voyageid", "suggestion", "revision", "iid", "adlt1imp", "adlt2imp", "adlt3imp", "adpsale1", "adpsale2", "adult1", "adult2", "adult3", "adult4", "adult5", "adult6", "adult7", "arrport", "arrport2", "boy1", "boy2", "boy3", "boy4", "boy5", "boy6", "boy7", "boyrat1", "boyrat3", "boyrat7", "captaina", "captainb", "captainc", "chil1imp", "chil2imp", "chil3imp", "child1", "child2", "child3", "child4", "child5", "child6", "child7", "chilrat1", "chilrat3", "chilrat7", "constreg", "crew", "crew1", "crew2", "crew3", "crew4", "crew5", "crewdied", "datedepa", "datedepb", "datedepc", "d1slatra", "d1slatrb", "d1slatrc", "dlslatra", "dlslatrb", "dlslatrc", "ddepam", "ddepamb", "ddepamc", "datarr32", "datarr33", "datarr34", "datarr36", "datarr37", "datarr38", "datarr39", "datarr40", "datarr41", "datarr43", "datarr44", "datarr45", "datedep", "datebuy", "dateleftafr", "dateland1", "dateland2", "dateland3", "datedepam", "dateend", "deptregimp", "deptregimp1", "embport", "embport2", "embreg", "embreg2", "evgreen", "fate", "fate2", "fate3", "fate4", "female1", "female2", "female3", "female4", "female5", "female6", "female7", "feml1imp", "feml2imp", "feml3imp", "girl1", "girl2", "girl3", "girl4", "girl5", "girl6", "girl7", "girlrat1", "girlrat3", "girlrat7", "guns", "infant1", "infant3", "infant4", "jamcaspr", "majbuypt", "majbyimp", "majbyimp1", "majselpt", "male1", "male1imp", "male2", "male2imp", "male3", "male3imp", "male4", "male5", "male6", "male7", "malrat1", "malrat3", "malrat7", "men1", "men2", "men3", "men4", "men5", "men6", "men7", "menrat1", "menrat3", "menrat7", "mjbyptimp", "mjselimp", "mjselimp1", "mjslptimp", "natinimp", "national", "ncar13", "ncar15", "ncar17", "ndesert", "npafttra", "nppretra", "npprior", "ownera", "ownerb", "ownerc", "ownerd", "ownere", "ownerf", "ownerg", "ownerh", "owneri", "ownerj", "ownerk", "ownerl", "ownerm", "ownern", "ownero", "ownerp", "plac1tra", "plac2tra", "plac3tra", "placcons", "placreg", "portdep", "portret", "ptdepimp", "regarr", "regarr2", "regdis1", "regdis2", "regdis3", "regem1", "regem2", "regem3", "regisreg", "resistance", "retrnreg", "retrnreg1", "rig", "saild1", "saild2", "saild3", "saild4", "saild5", "shipname", "sla1port", "slaarriv", "sladafri", "sladamer", "sladvoy", "slamimp", "slas32", "slas36", "slas39", "slavema1", "slavema3", "slavema7", "slavemx1", "slavemx3", "slavemx7", "slavmax1", "slavmax3", "slavmax7", "slaximp", "slinten2", "slintend", "sourcea", "sourceb", "sourcec", "sourced", "sourcee", "sourcef", "sourceg", "sourceh", "sourcei", "sourcej", "sourcek", "sourcel", "sourcem", "sourcen", "sourceo", "sourcep", "sourceq", "sourcer", "tonmod", "tonnage", "tontype", "tslavesd", "tslavesp", "tslmtimp", "voy1imp", "voy2imp", "voyage", "vymrtimp", "vymrtrat", "women1", "women2", "women3", "women4", "women5", "women6", "women7", "womrat1", "womrat3", "womrat7", "xmimpflag", "year10", "year100", "year25", "year5", "yearaf", "yearam", "yeardep", "yrcons", "yrreg"};
+		List<String> arrList = Arrays.asList(str);  
+				
+		for (Iterator iterator = arrList.iterator(); iterator.hasNext();) {
+			String attr  = (String) iterator.next();
+			query.addPopulatedAttribute(Voyage.getAttribute(attr));
+		}
+		
+		return query;	
 	}
 	
 	@Test
@@ -167,14 +193,50 @@ public class DbExportTest extends TestCase{
 		session = HibernateConn.getSession();
 		tran = session.beginTransaction();
 		Object[] rslt = q.executeQuery(session);
+		HashMap voyMap = new HashMap(); 
 		for (int i=0; i < rslt.length; i++) {
-			Submission sub = (Submission)rslt[i];
-			EditedVoyage voy= ((SubmissionNew) sub).getNewVoyage();
-			String name = ((SubmissionNew) sub) .getUser().getUserName();
+			SubmissionNew sub = (SubmissionNew)rslt[i];
+			EditedVoyage voy= sub.getNewVoyage();
+			String name = sub.getUser().getUserName();
+			System.out.println("submission id:" + sub.getId());
 			System.out.println("voyage iid:" + voy.getVoyage().getIid());
+			System.out.println("shipname:" + voy.getVoyage().getShipname());
 			System.out.println("user name:" + name);
+			EditedVoyage editVoy= sub.getEditorVoyage();
+			if (editVoy != null) {
+				System.out.println("edit voyage iid:" + editVoy.getVoyage().getIid());
+				System.out.println("edit shipname:" + editVoy.getVoyage().getShipname());
+				System.out.println("edit voyageid:" + editVoy.getVoyage().getVoyageid());				
+				List voyages = session.createCriteria(Voyage.class).add(Restrictions.eq("voyageid", editVoy.getVoyage().getVoyageid())).list();
+				for (int j=0; j < voyages.size(); j++){
+					Voyage voyg = (Voyage)voyages.get(j); 
+					System.out.println("voyg iid:" + voyg.getIid());
+					System.out.println("voyg shipname:" + voyg.getShipname());
+					System.out.println("voyg voyageid:" + voyg.getVoyageid());
+					voyMap.put(voyg.getIid(), name);
+				}				
+			}			
 			
 		}
+		System.out.println("voyage map: " + voyMap.toString());
+		//find all voyage ids associated with a submission
+		
+		/*TastDbQuery q = new TastDbQuery(new String("SubmissionEdit"));
+		session = HibernateConn.getSession();
+		tran = session.beginTransaction();
+		Object[] rslt = q.executeQuery(session);
+		for (int i=0; i < rslt.length; i++) {
+			Submission sub = (Submission)rslt[i];
+			String name = ((SubmissionEdit) sub) .getUser().getUserName();
+			System.out.println("user name:" + name);
+			EditedVoyage newVoy= ((SubmissionEdit) sub).getNewVoyage();
+			System.out.println("new voyage iid:" + newVoy.getVoyage().getIid());;
+			EditedVoyage voy= ((SubmissionEdit) sub).getEditorVoyage();
+			System.out.println("edit voyage iid:" + voy.getVoyage().getIid());
+			EditedVoyage oldVoy= ((SubmissionEdit) sub).getOldVoyage();
+			System.out.println("old voyage iid:" + oldVoy.getVoyage().getIid());		
+			
+		}*/
 	
 	}
 	
@@ -182,7 +244,7 @@ public class DbExportTest extends TestCase{
 	public void testAdminBean() {
 		AdminSubmissionBean bean = new AdminSubmissionBean();		
 		bean.getFileAllData();
-	}
+	}	
 	
 	@Test
 	public void testAccentedCharacters(){
@@ -261,6 +323,5 @@ public class DbExportTest extends TestCase{
 	         try { bw.close(); fos.close(); } catch (Exception ex) { }
 	      }
 	   }
-
-
+		
 }
