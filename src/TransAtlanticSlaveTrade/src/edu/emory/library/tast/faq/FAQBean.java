3bfc0ec5bb4d766408a8fdbd33c6a88c39a7e76d
Alex Thomas <athom09@emory.edu>
2010-04-09 14:04:13 +0000

    merging branch release2 r2259:2269 into trunk: added GPL to all 552 java files
    git-svn-id: https://svn.library.emory.edu/svn/slavevoyages/trunk@2270 2440b244-9809-4ec7-bebd-234906d9abf5

--- src/TransAtlanticSlaveTrade/src/edu/emory/library/tast/faq/FAQBean.java
+++ src/TransAtlanticSlaveTrade/src/edu/emory/library/tast/faq/FAQBean.java
@@ -1,3 +1,21 @@
+/*
+Copyright 2010 Emory University
+	
+	    This file is part of Trans-Atlantic Slave Voyages.
+	
+	    Trans-Atlantic Slave Voyages is free software: you can redistribute it and/or modify
+	    it under the terms of the GNU General Public License as published by
+	    the Free Software Foundation, either version 3 of the License, or
+	    (at your option) any later version.
+	
+	    Trans-Atlantic Slave Voyages is distributed in the hope that it will be useful,
+	    but WITHOUT ANY WARRANTY; without even the implied warranty of
+	    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	    GNU General Public License for more details.
+	
+	    You should have received a copy of the GNU General Public License
+	    along with Trans-Atlantic Slave Voyages.  If not, see <http://www.gnu.org/licenses/>. 
+*/
 package edu.emory.library.tast.faq;
 
 import java.util.Iterator;
